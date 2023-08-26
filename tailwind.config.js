a26ea00774faa93a2abd184e0e5ba84b94878aa7
Brandon.DeVore <Brandon.DeVore@publix.com>
2023-08-26 07:54:54 -0400

    change of theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
 module.exports = {
-  plugins: [
-    require('daisyui')
-  ],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: [
       {
         docuseal: {
           'color-scheme': 'light',
           primary: '#e4e0e1',
-          secondary: '#ef9fbc',
+          secondary: '#012D55',
           accent: '#eeaf3a',
-          neutral: '#291334',
+          neutral: '#AB2524',
           'base-100': '#faf7f5',
           'base-200': '#efeae6',
           'base-300': '#e7e2df',
           'base-content': '#291334',
           '--rounded-btn': '1.9rem',
           '--tab-border': '2px',
-          '--tab-radius': '.5rem'
-        }
-      }
-    ]
-  }
+          '--tab-radius': '.5rem',
+        },
+      },
+    ],
+  },
 }
